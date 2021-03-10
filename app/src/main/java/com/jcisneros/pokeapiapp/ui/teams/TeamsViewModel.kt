9a6f3d9f04a1fed1f22fc06f5242f99bfd093b64
jCisneros <anju.jjcg@gmail.com>
2021-03-10 12:06:56 -0600

    update: show list of teams

--- app/src/main/java/com/jcisneros/pokeapiapp/ui/teams/TeamsViewModel.kt
+++ app/src/main/java/com/jcisneros/pokeapiapp/ui/teams/TeamsViewModel.kt
@@ -1,10 +1,16 @@
 package com.jcisneros.pokeapiapp.ui.teams
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.Query
 import com.jcisneros.pokeapiapp.datasource.firebasedatasource.FirebaseService
+import com.jcisneros.pokeapiapp.datasource.models.Team
 import com.jcisneros.pokeapiapp.datasource.models.User
 import com.jcisneros.pokeapiapp.datasource.models.UserTeam
 import com.jcisneros.poketeamsapp.datasources.entities.PokemonInfo
@@ -12,6 +18,13 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class TeamsViewModel : ViewModel() {
+
+
+    //array
+    val arrayTeam = ArrayList<UserTeam>()
+
+    //teams pokemon of user
+    var teamsPokemonUserList = MutableLiveData<List<UserTeam>>()
 
     //teams pokemon of user
     var teamsPokemonUser = MutableLiveData<UserTeam>()
@@ -24,19 +37,40 @@ class TeamsViewModel : ViewModel() {
         datasource = FirebaseService()
     }
 
-    //get moshi instance
-    private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
 
     //method for get teams of user
     fun getAllTeams(user: User){
         viewModelScope.let {
-            datasource.getTeams(user)
-            //todo: corvert this data for dysplay teams in UI
-//            teamsPokemonUser.value = datasource.getTeams(user) as MutableList<UserTeam>
+            datasource.getAllTeams(user)
+            teamsPokemonUserList.postValue(arrayTeam)
 
         }
     }
 
+    //firestore db instance
+    private val db = FirebaseFirestore.getInstance()
+
+    fun getAllTeamsFirebase(user: User) {
+        db.collection("users").document(user.emal)
+            .collection("teams")
+            .get().addOnSuccessListener { res->
+                for (document in res){
+                    //get pokemons of team
+                    val listOfPokemon = listOf<PokemonInfo>()
+                    //var teamsPokemonUser = MutableLiveData<UserTeam>()
+                        val userTeam = UserTeam(
+                            document.data.get("teamName").toString(),
+                            document.data.get("teamToken").toString(),
+                            listOfPokemon
+                        )
+                    if(res.size()!=arrayTeam.size){
+                        arrayTeam.add(userTeam)
+                    }
+
+                }
+            }
+
+    }
+
 }
+
